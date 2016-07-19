2091718ae9fbfa0e9b14014c25a4cdffb7558b65
Joris Bontje <jorisbontje@gmail.com>
2016-07-19 16:13:20 +0200

    log when I forget to provide settings

--- frontend/server/main.js
+++ frontend/server/main.js
@@ -2,4 +2,7 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  if (_.isEmpty(Meteor.settings.public)) {
+    throw new Meteor.Error(500, 'Settings not available');
+  }
 });
