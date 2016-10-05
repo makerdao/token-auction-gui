1359f8f70f265f93f10eac2ce9bcd495a80f7cec
sunfeld <sunfeld@sunis.nl>
2016-10-05 10:07:38 +0200

    added balance blocks.

---
+++ frontend/imports/client/headers/balance.js
@@ -0,0 +1,14 @@
+import { Template } from 'meteor/templating';
+import Tokens from '/imports/api/tokens.js';
+import './balance.html';
+
+Template.balance.viewmodel({
+  hasETHTokens() {
+    const token = Tokens.findOne({ name: 'ETH' });
+    console.log(token);
+    if (token !== undefined) {
+      return web3.toBigNumber(token.balance).gt(0);
+    }
+    return false;
+  },
+});
