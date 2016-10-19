4e83d1c1f11b134cdc40f63cbc01663b441f811e
Mariano Conti <nanexcool@gmail.com>
2016-10-19 13:41:42 -0300

    Get url via window location

--- frontend/imports/client/share.js
+++ frontend/imports/client/share.js
@@ -1,13 +1,10 @@
 import { Template } from 'meteor/templating';
-import Auctions from '/imports/api/auctions.js';
 import './share.html';
 
 Template.share.viewmodel({
   shareUrl() {
-    // TODO: get real url
-    const url = 'https://makerdao.github.io/weekly-mkr-auction';
-    const singleAuction = Auctions.findAuction();
-    return `${url}/#!/auction/${singleAuction.auctionId}`;
+    const url = window.location.href;
+    return url;
   },
   copy() {
     const el = document.querySelector('#shareTextbox');
