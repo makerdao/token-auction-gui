9ec92030b11c03c59d3dac76999e91aacc3a993a
reverendus <reverendus@invalid>
2017-05-06 19:05:58 +0200

    Account selector and active account address removed

--- frontend/imports/client/accountselector.js
+++ frontend/imports/client/accountselector.js
@@ -1,30 +1,8 @@
 import { Session } from 'meteor/session';
 import { Template } from 'meteor/templating';
 
-import Tokens from '/imports/api/tokens';
-import clearMessages from '/imports/utils/clearmessages.js';
-
 import './accountselector.html';
 
-Template.accountSelector.helpers({
-  accounts() {
-    return Session.get('accounts');
-  },
-});
-
 Template.accountSelector.viewmodel({
-  account: () => Session.get('address'),
   contract: () => Session.get('contractAddress'),
-});
-
-Template.accountSelector.events({
-  change(event) {
-    Session.set('address', event.target.value);
-    localStorage.setItem('address', event.target.value);
-    web3.eth.defaultAccount = event.target.value;
-
-    // Remove messages from UI
-    clearMessages();
-    Tokens.sync();
-  },
 });
