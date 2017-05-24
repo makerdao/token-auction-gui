ee7524c608813349b758dd0ad54dc3130a4b1dd9
reverendus <reverendus@invalid>
2017-05-24 13:31:07 +0200

    Some participant and token addresses updated - for testing purposes

--- frontend/imports/api/participants.js
+++ frontend/imports/api/participants.js
@@ -8,7 +8,7 @@ const participants = {
     '0x002ca7f9b416b2304cdd20c26882d1ef5c53f611': 'reverendus',
     '0x0061f1dbaf1e1b2e412a75d3ed6b48c3d7412d35': 'reverendus2',
     '0x0046f01ad360270605e0e5d693484ec3bfe43ba8': 'reverendus3',
-    '0x00348e4084567ce2e962b64abe5a54bab256bc26': 'DAI Stablecoin System',
+    '0x00348e4084567ce2e962b64abe5a54bab256bc26': 'reverendus4',
   },
   live: {
     //TBD
--- frontend/imports/api/tokens.js
+++ frontend/imports/api/tokens.js
@@ -5,16 +5,16 @@ const Tokens = new Mongo.Collection(null);
 
 const tokens = {
   kovan: {
-    ETH: '0x53eccc9246c1e537d79199d0c7231e425a40f896',
-    DAI: '0x383105dc3dab1646119335ae54afdfd9f2af4713', //TODO experiment by reverendus, there is no DAI yet :)
-    MKR: '0x4bb514a7f83fbb13c2b41448208e89fabbcfe2fb',
-    MKP: '0x408f224724a7680b6172bd37bf482c50e2be5d02', //TODO experiment by reverendus, to be removed later
+    'ETH': '0x53eccc9246c1e537d79199d0c7231e425a40f896',
+    'DAI': '0x0000000000000000000000000000000000000000',
+    'MKR': '0x4bb514a7f83fbb13c2b41448208e89fabbcfe2fb',
+    'REVERENDUS-DAI': '0x383105dc3dab1646119335ae54afdfd9f2af4713',
+    'REVERENDUS-SAI': '0x408f224724a7680b6172bd37bf482c50e2be5d02',
   },
   live: {
-    ETH: '0xecf8f87f810ecf450940c9f60066b4a7a501d6a7',
-    DAI: '0x0000000000000000000000000000000000000000',
-    MKR: '0xc66ea802717bfb9833400264dd12c2bceaa34a6d',
-    MKP: '0x0000000000000000000000000000000000000000', //TODO experiment by reverendus, to be removed later
+    'ETH': '0xecf8f87f810ecf450940c9f60066b4a7a501d6a7',
+    'DAI': '0x0000000000000000000000000000000000000000',
+    'MKR': '0xc66ea802717bfb9833400264dd12c2bceaa34a6d',
   },
 };
 
